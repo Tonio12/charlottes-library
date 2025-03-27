781ca4c316ef3a077bea74ebb95201e1b5c2ea39
Antonio Nelson <nelson.antonio.an@gmail.com>
2025-03-27 14:47:28 +0000

    make admin route protected

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,11 +4,22 @@ import React from 'react'
 import '@/src/styles/admin.css'
 import Sidebar from '@/src/components/admin/Sidebar'
 import Header from '@/src/components/admin/Header'
-
+import { db } from '@/database/drizzle'
+import { usersTable } from '@/database/schema'
+import { eq } from 'drizzle-orm'
 const layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth()
 
   if (!session?.user?.id) redirect('/sign-in')
+
+  const isAdmin = await db
+    .select({ isAdmin: usersTable.role })
+    .from(usersTable)
+    .where(eq(usersTable.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0].isAdmin === 'ADMIN')
+
+  if (!isAdmin) redirect('/')
 
   return (
     <main className="flex min-h-screen w-full">
