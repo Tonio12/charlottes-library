7bf921bf717dbfde563ba05a921ebd71c98161ba
Antonio Nelson <nelson.antonio.an@gmail.com>
2025-04-01 15:49:42 +0000

    fixing error token not found

--- src/lib/workflow.ts
+++ src/lib/workflow.ts
@@ -3,7 +3,6 @@ import config from './config'
 import { Client as QstashClient, resend } from '@upstash/qstash'
 
 export const workflowClient = new WorkflowClient({
-  baseUrl: process.env.QSTASH_URL!,
   token: process.env.QSTASH_TOKEN!,
 })
 
