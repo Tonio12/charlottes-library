9394c0ef542dd12543876090e08ba74e77617bb6
Antonio Nelson <nelson.antonio.an@gmail.com>
2025-04-01 13:54:49 +0000

    attempt to fix qstash error

--- src/lib/workflow.ts
+++ src/lib/workflow.ts
@@ -3,8 +3,8 @@ import config from './config'
 import { Client as QstashClient, resend } from '@upstash/qstash'
 
 export const workflowClient = new WorkflowClient({
-  baseUrl: config.env.upstash.qstashUrl,
-  token: config.env.upstash.qstashToken,
+  baseUrl: process.env.QSTASH_URL!,
+  token: process.env.QSTASH_TOKEN!,
 })
 
 const qstashClient = new QstashClient({ token: process.env.QSTASH_TOKEN! })
