0648af939e3f3e32d3702772a06fed3d2cb859e1
Antonio Nelson <nelson.antonio.an@gmail.com>
2025-04-03 11:15:33 +0000

    attempt to fix qstash error

--- src/lib/workflow.ts
+++ src/lib/workflow.ts
@@ -4,12 +4,14 @@ import config from './config'
 // Debug token loading
 console.log(
   'QStash Token:',
-  config.env.upstash.qstashToken ? 'Token exists' : 'Token is missing'
+  config.env.upstash.qstashCurrentSigningToken
+    ? 'Token exists'
+    : 'Token is missing'
 )
 
 // Create a function to get the workflow client
 export const getWorkflowClient = () => {
   return new WorkflowClient({
-    token: config.env.upstash.qstashToken,
+    token: config.env.upstash.qstashCurrentSigningToken,
   })
 }
